37235c75790d02dc0d3e2739ec7ede1e16b90b97
Defteggg <leandre.castagna@epfl.ch>
2021-10-23 16:04:18 +0200

    testlogistic

---
+++ project1/scripts/logistic.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+def sigmoid(t):
+    """apply the sigmoid function on t."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO
+    ind_pos=np.where(t>=0)
+    ind_neg=np.where(t<0)
+    t[ind_pos]=1/(1+np.exp(-t[ind_pos]))
+    t[ind_neg]=np.exp(t[ind_neg])/(1+np.exp(t[ind_neg]))
+    return t
+    
+def calculate_loss(y, tx, w):
+    """compute the loss: negative log likelihood."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO
+    Xw=tx@w
+    print(sigmoid(Xw))
+    return np.sum(y*np.log(sigmoid(Xw))+(np.ones(np.shape(y))-y)*np.log(np.ones(np.shape(y))-sigmoid(Xw)))
+    
+    
+    
+
+def calculate_gradient(y, tx, w):
+    """compute the gradient of loss."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO
+    d=y-sigmoid(tx@w)
+    print(d)
+    #l=d*tx
+    return -tx.T@d#-np.sum(l.T,axis=1)
+
+
+def learning_by_gradient_descent(y, tx, w, gamma):
+    """
+    Do one step of gradient descent using logistic regression.
+    Return the loss and the updated w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # compute the loss: TODO
+    loss=calculate_loss(y,tx,w)
+    # ***************************************************
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # compute the gradient: TODO
+    g=calculate_gradient(y,tx,w)
+    print(np.linalg.norm(g))
+    # ***************************************************
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # update w: TODO
+    w=w-gamma*g
+    # ***************************************************
+    return loss, w
+
+
+def logistic_regression(y, tx, w):
+    """return the loss, gradient, and Hessian."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # return loss, gradient, and Hessian: TODO
+    loss=calculate_loss(y,tx,w)
+    gradient=calculate_gradient(y,tx,w)
+    Hessian=calculate_hessian(y,tx,w)
+    return loss,gradient,Hessian
+
+
+def logistic_regression_gradient_descent(y,tx,gamma,max_iter,threshold,w):
+    # init parameters
+    losses = []
+
+    # start the logistic regression
+    for iter in range(max_iter):
+        # get loss and update w.
+        loss, w = learning_by_gradient_descent(y, tx, w, gamma)
+        # log info
+        print(loss)
+        if iter % 100 == 0:
+            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
+        # converge criterion
+        losses.append(loss)
+        #if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+         #   break
+    return w,losses[iter]
