bb8038dc1c74efac6662ec282eecf71e4f2e687a
salimajaoua <79655397+salimajaoua@users.noreply.github.com>
2021-11-01 13:18:27 +0100

    Delete project1/scripts directory

--- project1/scripts/standardize.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def standardize(x):
-    ''' fill your code in here...
-    '''
-    centered_data = x - np.mean(x, axis=0)
-    std_data = centered_data / np.std(centered_data, axis=0)
-    
-    return std_data
